ee8a42f2891f6f5da2a0d42eb85ca2b44e18355a
nilldiggonto <nilldiggonto@gmail.com>
2020-11-07 23:54:50 +0600

    View Created

--- images/views.py
+++ images/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
+from .models import Image
 # Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(data= request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request,'Image Uploaded Successfully')
+            return redirect(new_item.get_absolute_url())
+        
+        else:
+            form = ImageCreateForm(data=request.GET)
+    else:
+        form = ImageCreateForm(data=request.GET)
+
+    return render(request,template_name,{'section':images,'form':form})
